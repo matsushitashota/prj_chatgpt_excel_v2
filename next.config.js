249b976157905befa3ebedbeab168936715b7716
matsushitashota <matsushita.shota@lafool.co.jp>
2024-01-02 10:11:11 +0900

    add headers Cross-Origin-Opener-Policy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin" // "same-origin-allow-popups"
+          }
+        ]
+      }
+    ]
   }
 }
 
